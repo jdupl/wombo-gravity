adf615d2316e583012bada224492abf712c09861
Hugo Côté <coteh@hotmail.fr>
2014-12-23 16:05:01 -0500

    Essaie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Happy feet");
 		System.out.println("Wombo combo");
+		System.out.println("What in the world");
 	}
 
 }
