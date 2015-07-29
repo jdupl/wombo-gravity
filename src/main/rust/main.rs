1dae28c8ac9ad8a7b0e7da8dee167098623b4a26
Justin Duplessis <drfoliberg@gmail.com>
2015-07-29 12:17:16 -0400

    Implement calls to accel()

--- src/main/rust/main.rs
+++ src/main/rust/main.rs
@@ -5,16 +5,22 @@ use std::fs::File;
 use std::path::Path;
 use std::io::Read;
 
+const G: f64 = -6.6738480e-11;
+
 #[derive(Debug)]
 pub struct Body {
     id: u32,
     name: String,
+    m: f64,
+    // position
     rx: f64,
     ry: f64,
     rz: f64,
+    // velocity
     vx: f64,
     vy: f64,
     vz: f64,
+    // acceleration
     ax: f64,
     ay: f64,
     az: f64
@@ -23,15 +29,83 @@ pub struct Body {
 fn main() {
     let max_frames = 50;
     // How many computations in a frame
-    let instant_count = 60 * 60 * 24;
+    let instant_count = 24 * 60 * 60;
 
     // Duration in second of an instant
     let instant_duration_sec = 1;
 
-    let bodies = read_json("data/data.json");
+    let mut bodies = read_json("data/data.json");
 
-    for body in bodies.iter() {
-        println!("{:?}", body);
+    let mut current_frame = 0;
+
+    while current_frame < max_frames {
+        current_frame += 1;
+        compute_frame(instant_count, instant_duration_sec, &mut bodies);
+    }
+}
+
+fn compute_frame(instant_count: u32, instant_duration_sec: u32, bodies: &mut Vec<Body>) {
+    let mut instant_counter = 0;
+
+    while instant_counter < instant_count {
+        instant_counter += 1;
+        compute_instant(instant_duration_sec, bodies);
+    }
+}
+
+fn compute_instant(instant_duration_sec: u32, bodies: &mut Vec<Body>) {
+    let body_count = bodies.len();
+
+    let mut i = 0;
+    let mut j = 0;
+
+    while i < body_count {
+        j = i + i;
+        while j < body_count {
+            accel(bodies, i, j);
+            j += 1;
+        }
+        i += 1;
+    }
+
+    for mut body in bodies.iter_mut() {
+        body.ax *= G;
+        body.ay *= G;
+        body.az *= G;
+
+        // body.update_pos();
+        // body.ax = 0;
+        // body.ay = 0;
+        // body.az = 0;
+    }
+}
+
+
+fn accel(bodies: &mut Vec<Body>, b1_index: usize, b2_index: usize) {
+    let mut tmp_x = bodies[b1_index].rx - bodies[b2_index].rx;
+    let mut tmp_y = bodies[b1_index].ry - bodies[b2_index].ry;
+    let mut tmp_z = bodies[b1_index].rz - bodies[b2_index].rz;
+
+    let mut tmp = tmp_x * tmp_x + tmp_y * tmp_y + tmp_z * tmp_z;
+
+    tmp = ((bodies[b1_index].m) * (bodies[b2_index].m)) / (tmp.sqrt());
+
+    tmp_x *= tmp;
+    tmp_y *= tmp;
+    tmp_z *= tmp;
+
+    {
+        let ref mut b1 = &mut bodies[b1_index];
+        b1.ax += tmp_x;
+        b1.ay += tmp_y;
+        b1.az += tmp_z;
+    }
+
+    {
+        let ref mut b2 = &mut bodies[b2_index];
+        b2.ax -= tmp_x;
+        b2.ay -= tmp_y;
+        b2.az -= tmp_z;
     }
 }
 
@@ -39,6 +113,7 @@ pub fn read_json(name: &str) -> Vec<Body> {
     #[derive(RustcDecodable, RustcEncodable, Debug)]
     struct BodyData {
         name: String,
+        m: f64,
         rx: f64,
         ry: f64,
         rz: f64,
@@ -69,11 +144,11 @@ pub fn read_json(name: &str) -> Vec<Body> {
 
     for body_data in decoded.bodies.iter() {
         let mut body = Body {
-            id: count, name: body_data.name.clone(), rx: body_data.rx, ry: body_data.ry,
-            rz: body_data.rz, vx: body_data.vx, vy: body_data.vy, vz: body_data.vz,
-            ax: 0.0, ay: 0.0, az: 0.0
+            id: count, name: body_data.name.clone(), m: body_data.m, rx: body_data.rx,
+            ry: body_data.ry, rz: body_data.rz, vx: body_data.vx, vy: body_data.vy,
+            vz: body_data.vz, ax: 0.0, ay: 0.0, az: 0.0
         };
-        
+
         count += 1;
         bodies.push(body);
     }
