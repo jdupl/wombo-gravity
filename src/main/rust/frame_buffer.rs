c4cd3671364f0f6fb7806e207b900ff4ba6d4ee7
Justin Duplessis <drfoliberg@gmail.com>
2015-07-29 20:32:42 -0400

    use max count for rust frame buffer

--- src/main/rust/frame_buffer.rs
+++ src/main/rust/frame_buffer.rs
@@ -6,6 +6,7 @@ use writer::Writer;
 pub struct FrameBuffer<T> {
     writer: T,
     frames: VecDeque<Frame>,
+    max_count: usize,
 }
 
 impl<T: Writer> FrameBuffer<T> {
@@ -13,12 +14,13 @@ impl<T: Writer> FrameBuffer<T> {
         FrameBuffer {
             frames: VecDeque::<Frame>::new(),
             writer: writer,
+            max_count: 10,
         }
     }
 
     pub fn add(&mut self, frame: Frame) {
         self.frames.push_back(frame);
-        if self.frames.len() > 10 {
+        if self.frames.len() >= self.max_count {
             self.writer.flush(&mut self.frames);
         }
     }
